55bda959c50831ee473e0deaa8cd5ed72c9561ed
InnaB <isbocha@gmail.com>
2023-06-01 21:43:53 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int x = 17_240; //Сумма потраченных рублей
-        int y = 20; //Стоимость одной мили
-        int z = x / y; //Колличество полученных миль
+        int money = 17_240; //Сумма потраченных рублей
+        int priceBonus = 20; //Стоимость одной мили
+        int bonus = money / priceBonus; //Колличество полученных миль
         System.out.println("Колличество полученных миль:");
-        System.out.println(z);
+        System.out.println(bonus);
     }
 }
